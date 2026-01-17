1f1c15277084a36c4ce2cdf220479fa3b6297184
caiqian <3511672@qq.com>
2026-01-17 13:21:54 +0800

    init

---
+++ study/main.py
@@ -0,0 +1,15 @@
+def main():
+    print("Hello World")
+
+    list_a = [1, 2, 3, 4]
+
+    print(list_a[0])  # 第一个元素
+    print(list_a[-1])  # 最后一个元素
+
+    list_a[1] = 99  # 修改第二个元素
+    print(list_a)
+
+
+if __name__ == "__main__":
+    main()
+
